0e5606cee7ae57351883555de388c711bed029c2
Erika Yardumian <eyardumian@hotmail.com>
2019-09-28 18:09:20 -0700

    Added comments

--- Employee/Form1.cs
+++ Employee/Form1.cs
@@ -19,14 +19,18 @@ namespace Employee
 
         private void EnterButton_Click(object sender, EventArgs e)
         {
+            //Create an object of the ProductionWorker class
             ProductionWorker worker = new ProductionWorker();
+
+            //Retrieve the object's properties
             worker.EmployeeName = EmployeeNameTextBox.Text;
             worker.EmployeeNumber = (Int32.Parse(EmployeeNumberTextBox.Text));
             worker.ShiftNumber = (Int32.Parse(comboBox1.Text));
             worker.HourlyPayRate = (Double.Parse(PayRateTextBox.Text));
 
-            MessageBox.Show("Name: " + worker.EmployeeName + "Number: " + worker.EmployeeNumber
-                + "Shift: " + worker.ShiftNumber + "Pay Rate: " + worker.HourlyPayRate);
+            //Display object values
+            MessageBox.Show("Name: " + worker.EmployeeName + "\n" + "Number: " + worker.EmployeeNumber
+                + "\n" + "Shift: " + worker.ShiftNumber + "\n" + "Pay Rate: $" + worker.HourlyPayRate);
         }
     }
 }
